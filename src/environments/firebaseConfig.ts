c8c1293580b005b1479b13a5953dcf60322156ef
manojmallick <manoj.mallick079@gmail.com>
2019-11-18 22:57:44 +0100

    Update firebaseConfig.ts

--- src/environments/firebaseConfig.ts
+++ src/environments/firebaseConfig.ts
@@ -1,8 +1,10 @@
 export const FireBaseConfig = {
   apiKey: "AIzaSyDm0idQQ7yN90jlqp6m0lrnk1TohC7Mxh4",
   authDomain: "shopping-cart-1d0fe.firebaseapp.com",
-  databaseURL: "/shopping/er7sDYnU8lhFZU1y4Gpt",
+  databaseURL: "https://shopping-cart-1d0fe.firebaseio.com",
   projectId: "shopping-cart-1d0fe",
-  storageBucket: "shopping-cart-1d0fe",
-  messagingSenderId: "test"
-};
+  storageBucket: "shopping-cart-1d0fe.appspot.com",
+  messagingSenderId: "26418879934",
+  appId: "1:26418879934:web:47e217151ac7642c656def",
+  measurementId: "G-F9G3TELB27"
+};;
